3461a636c9e915d452fa4ff0008fd45c6149dffa
raihanbashir <raihanbash@gmail.com>
2024-02-03 23:22:06 +0530

    Bug fixes for eslint

--- netflix-ui/.eslintrc.js
+++ netflix-ui/.eslintrc.js
@@ -3,6 +3,14 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
+
+
+    "settings": {
+        "react": {
+        "version": "detect"
+        }
+    },
+
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended"
