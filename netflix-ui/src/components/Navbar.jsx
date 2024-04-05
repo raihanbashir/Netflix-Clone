b8b1b41a85fcbf7bb5ed4ecb4d07a1baf92211c6
raihanbashir <raibash04@gmail.com>
2024-04-05 16:52:46 +0530

    Navbar and Main Page

--- netflix-ui/src/components/Navbar.jsx
+++ netflix-ui/src/components/Navbar.jsx
@@ -1,156 +1,158 @@
 /* eslint-disable react/prop-types */
-import React, {useState} from "react";
-import styled from "styled-components"
-import logo from "../assets/logo.png"
+import React, { useState } from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import {FaSearch} from "react-icons/fa";
-import { FaPowerOff } from "react-icons/fa";
-import { firebaseAuth } from "../utils/firebase-config";
-import { signOut } from "firebase/auth";
-export default function Navbar({isScrolled}) {
+import { FaSearch, FaPowerOff } from "react-icons/fa";
+import { firebaseAuth } from "../utils/firebase-config"; // Check if this is imported correctly
+import { onAuthStateChanged,signOut } from "firebase/auth"; // Check if this import is correct
+import { useEffect } from "react";
+import{useNavigate} from "react-router-dom";
+
+
+
+export default function Navbar({ isScrolled }) {
+
+  const navigate = useNavigate();
+
 
     const links = [
-        {name : "Home",link : "/"},
-        {name : "TV Shows",link : "/tv"},
-        {name : "Movies",link : "/movies"},
-        {name : "My List",link : "/"}
-    ]
+        { name: "Home", link: "/" },
+        { name: "TV Shows", link: "/tv" },
+        { name: "Movies", link: "/movies" },
+        { name: "My List", link: "/" }
+    ];
 
-    const [showSearch,setShowSearch] = useState(false);
-    const [inputHover,setinputHover] = useState(false);
+    useEffect(() => {
+      onAuthStateChanged(firebaseAuth,(currentUser)=> {
+          if(!currentUser) navigate("/login");
+      });
+  }, []);
 
-   return <Container>
-    <nav className={`flex ${isScrolled ? "scrolled" : ""}`}>
-        <div className="left flex a-center">
-            <div className="brand flex a-center j-center">
-                <img src = {logo} alt = "logo" />
-            </div>
-            <ul className="links flex">
-                {
-                    links.map(({name, link})=>{
-                        return (
-                            <li key = {name}>
+    const [showSearch, setShowSearch] = useState(false);
+
+    const handleSearchButton = () => {
+        setShowSearch(!showSearch);
+    };
+
+    const handleSignOut = () => {
+        signOut(firebaseAuth); // Call signOut function properly
+    };
+
+    return (
+        <Container>
+            <nav className={`flex ${isScrolled ? "scrolled" : ""}`}>
+                <div className="left flex a-center">
+                    <div className="brand flex a-center j-center">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <ul className="links flex">
+                        {links.map(({ name, link }) => (
+                            <li key={name}>
                                 <Link to={link}>{name}</Link>
                             </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-        <div className="right flex a-center">
-            <div className={`search ${showSearch ? "show-search":""}`}>
-                <button 
-                onFocus = {() => setShowSearch(true)} 
-                onBlur = {()=>{
-                        if(!inputHover) setShowSearch(false);
-                    }}
-                >
-                    <FaSearch />
-                </button>
-                <input type="text" placeholder= "Search"
-                onMouseEnter={()=> setinputHover(true)}
-                onMouseLeave={() => setinputHover(false)}
-                onBlur={()=> {
-                    setShowSearch(false);
-                    setinputHover(false);
-                }}
-                />
-            </div>
-            <button onClick = {()=> signOut(firebaseAuth)}>
-                <FaPowerOff />
-            </button>
-        </div>
-    </nav>
-   </Container>;
+                        ))}
+                    </ul>
+                </div>
+                <div className="right flex a-center">
+                    <div className={`search ${showSearch ? "show-search" : ""}`}>
+                        <button onClick={handleSearchButton}>
+                            <FaSearch />
+                        </button>
+                        <input type="text" placeholder="Search" />
+                    </div>
+                    <button onClick={handleSignOut}>
+                        <FaPowerOff />
+                    </button>
+                </div>
+            </nav>
+        </Container>
+    );
 }
 
 const Container = styled.div`
-  &.scrolled { /* Added '&' to target the component with the 'scrolled' class */
-    background-color: black;
-  }
-  nav {
-    position: sticky;
-    top: 0;
-    height: 6.5rem;
-    width: 100%;
-    justify-content: space-between;
-    /* Removed duplicate 'position: fixed;' */
-    z-index: 2;
-    padding: 0 4rem;
-    display: flex; /* Added to ensure flexbox properties work */
-    align-items: center;
-    transition: 0.3s ease-in-out;
-    .left {
-      gap: 2rem;
-      .brand {
-        img {
-          height: 4rem;
-        }
-      }
-      .links {
-        list-style-type: none;
-        display: flex; /* Added to ensure flexbox properties work */
-        gap: 2rem;
-        li {
-          a {
-            color: white;
-            text-decoration: none;
-          }
-        }
-      }
+    .scrolled {
+        background-color: black;
     }
-    .right {
-      gap: 1rem;
-      display: flex; /* Added to ensure flexbox properties work */
-      align-items: center; /* Added */
-      button {
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
-        &:focus {
-          outline: none;
-        }
-        svg {
-          color: #f34242;
-          font-size: 1.2rem;
-        }
-      }
-      .search {
+    nav {
+        position: sticky;
+        top: 0;
+        height: 6.5rem;
+        width: 100%;
+        justify-content: space-between;
+        position: fixed;
+        z-index: 2;
+        padding: 0 4rem;
         display: flex;
-        gap: 0.4rem;
         align-items: center;
-        justify-content: center;
-        padding: 0.2rem;
-        padding-left: 0.5rem;
-        button {
-          background-color: transparent;
-          svg {
-            color: white;
-          }
+        transition: 0.3s ease-in-out;
+
+        .left {
+            gap: 2rem;
+            .brand img {
+                height: 4rem;
+            }
+            .links {
+                list-style-type: none;
+                display: flex;
+                gap: 2rem;
+                li a {
+                    color: white;
+                    text-decoration: none;
+                }
+            }
         }
-        input {
-          width: 0;
-          opacity: 0;
-          visibility: hidden;
-          transition: 0.3s ease-in-out;
-          background-color: transparent;
-          border: none;
-          color: white;
-          &:focus {
-            outline: none;
-          }
+        .right {
+            gap: 1rem;
+            display: flex;
+            align-items: center;
+
+            button {
+                background-color: transparent;
+                border: none;
+                cursor: pointer;
+                &:focus {
+                    outline: none;
+                }
+                svg {
+                    color: #f34242;
+                    font-size: 1.2rem;
+                }
+            }
+
+            .search {
+                display: flex;
+                gap: 0.4rem;
+                align-items: center;
+                justify-content: center;
+                padding: 0.2rem;
+                padding-left: 0.5rem;
+                button svg {
+                    color: white;
+                }
+                input {
+                    width: 0;
+                    opacity: 0;
+                    visibility: hidden;
+                    transition: 0.3s ease-in-out;
+                    background-color: transparent;
+                    border: none;
+                    color: white;
+                    &:focus {
+                        outline: none;
+                    }
+                }
+                &.show-search {
+                    border: 1px solid white;
+                    background-color: rgba(0, 0, 0, 0.6);
+                    input {
+                        width: 100%;
+                        opacity: 1;
+                        visibility: visible;
+                        padding: 0.3rem;
+                    }
+                }
+            }
         }
-      }
-      &.show-search { /* Changed from '.show-search' to '&.show-search' to target the component with the 'show-search' class */
-        border: 1px solid white;
-        background-color: rgba(0, 0, 0, 0.6);
-        input {
-          width: 100%;
-          opacity: 1;
-          visibility: visible;
-          padding: 0.3rem;
-        }
-      }
     }
-  }
 `;
