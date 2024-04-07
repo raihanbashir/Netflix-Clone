3011ef17b0a2ee1f81ba4128444008640e2fee06
raihanbashir <raibash04@gmail.com>
2024-04-07 17:31:24 +0530

    Home Page finished

---
+++ netflix-ui/src/pages/Player.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+import { BsArrowLeft }from "react-icons/bs";
+import video from "../assets/"
+export default function Player() {
+   return <Container>Player</Container>;
+}
+const Container = styled.div``;
